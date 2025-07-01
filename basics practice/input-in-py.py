9a13761c48f43b502014e82a9ee78e7ea737b326
umairmalik7 <m.umair4983@gmail.com>
2025-07-01 18:46:02 +0500

    Add GFG solution: input in python

---
+++ basics practice/input-in-py.py
@@ -0,0 +1,16 @@
+#User function Template for python3
+########### Write your code below ###############
+
+# Take string input and print the string input
+word = input()
+print(word)
+
+# Take integer input and add 10 to the integer input and print
+num = int(input())
+print(num+10)
+
+# Take floating-point input and multiply the float input by 10 and print
+float_point = float(input())
+print(float_point *10)
+
+########### Write your code above ###############
