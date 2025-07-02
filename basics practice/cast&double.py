df2a32d81ed8f5891c037dba07050188b602f6ee
umairmalik7 <m.umair4983@gmail.com>
2025-07-02 16:10:14 +0500

    GFG solution: cast and double

---
+++ basics practice/cast&double.py
@@ -0,0 +1,7 @@
+num = input()
+
+########### Write your code below ###############
+# TypeCast num to int, double it and print
+x = int(num)
+print(x*2)
+########### Write your code above ###############
