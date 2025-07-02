1562e05c50ca4450f61afd76ba857aa40e4615f0
umairmalik7 <m.umair4983@gmail.com>
2025-07-02 13:54:29 +0500

    Add GFG solution: logical operators

---
+++ basics practice/logical-ope.py
@@ -0,0 +1,16 @@
+#User function Template for python3
+#The two lines below take input. Don't change them!
+a=int(input())
+b=int(input())
+
+########### Write your code below ###############
+#Do a and b below
+p = a and b
+#Do a or b below
+q = a or b
+#Do not a below
+r = not a
+########### Write your code above ###############
+
+#The code below prints the output. Don't change it!
+print(p,q,r)
