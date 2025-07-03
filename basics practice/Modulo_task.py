e543ff73a1cfc7706db571670dc9537ae5cece2f
umairmalik7 <m.umair4983@gmail.com>
2025-07-03 13:36:17 +0500

    GFG solution: Modulo task

---
+++ basics practice/Modulo_task.py
@@ -0,0 +1,10 @@
+#User function Template for python3
+
+class Solution:
+    def modTask(self, N):
+        # code here
+        if N == 1:
+            return N
+        else:
+            return N//2 +1
+    
