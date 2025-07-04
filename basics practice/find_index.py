f30358a0e5ce457573eda9f3a63af8854229c690
umairmalik7 <m.umair4983@gmail.com>
2025-07-04 13:12:50 +0500

    GFG solution: find index

---
+++ basics practice/find_index.py
@@ -0,0 +1,11 @@
+#User function Template for python3
+
+arr = tuple(map(int, input().split()))
+x = int(input())
+
+########### Write your code below ###############
+# Print the index of x in arr
+i = arr.index(x)
+print(i)
+
+########### Write your code above ###############
