90ebdcfb6149cf70ab2de6ce62db7ba032a379f9
umairmalik7 <m.umair4983@gmail.com>
2025-07-05 13:28:16 +0500

    GFG solution: GP term

---
+++ basics practice/GP_term.py
@@ -0,0 +1,11 @@
+#User function Template for python3
+a = int(input())
+n = int(input())
+r = 2
+
+########### Write your code below ###############
+# Compute the GP Term
+ans = a * (r**(n-1))
+########### Write your code above ###############
+
+print(ans)
