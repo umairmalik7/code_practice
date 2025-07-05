75632648a2b09c5567802a28a440738060328b34
umairmalik7 <m.umair4983@gmail.com>
2025-07-05 13:08:09 +0500

    GFG solution: Sqaure wall

---
+++ basics practice/square-wall.py
@@ -0,0 +1,9 @@
+#User function Template for python3
+n = int(input())
+
+# Your code here
+for i in range(n):
+    print("*", end=" ")
+    for i in range(n-1):
+        print("*", end=" ")
+    print()
