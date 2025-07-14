fdb9a9d2811f8b6a2a898feb6d3cd9fb21b1d653
umairmalik7 <m.umair4983@gmail.com>
2025-07-14 12:35:26 +0500

    GFG if&loops: if statement

---
+++ conditionals and loops practice/if_statement.py
@@ -0,0 +1,7 @@
+def friends_in_trouble(j_angry, s_angry):
+    if j_angry is True and s_angry is True:
+        return True
+    elif j_angry is False and s_angry is False:
+        return True
+    else:
+        return False
