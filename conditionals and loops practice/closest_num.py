83c6e51738cc22d32250b735101980c29e0afe7e
umairmalik7 <m.umair4983@gmail.com>
2025-07-19 23:20:19 +0500

    GFG i&loops: still trying

--- conditionals and loops practice/closest_num.py
+++ conditionals and loops practice/closest_num.py
@@ -5,22 +5,39 @@ mod = int(input("Enter mod: "))
 def closestNumber( n, m):
         # code here 
     if m != 0:
-        for i in range(n-1,0,-1):
+        for i in range(n,20,-1):
             if i % m == 0:
                 print(f"the value of i = {i}")
-                y  = math.isclose(i,n,rel_tol=0.4)
-                print(y)
+                y  = math.isclose(i,n,rel_tol=0.5)
                 if y == True: break
-        for j in range(n+1,n+10):
+        for j in range(n,n+20):
             if j % m == 0:
                 print(f"the value of j = {j}")
-                x = math.isclose(j,n,rel_tol=0.3)
-                print(x)
+                x = math.isclose(j,n,rel_tol=0.5)
                 if x == True: break
-        if i - n < j - n:
-            print(i)
-        else:
-            print(j)
+        
+        if n > 0:
+            if n - i > j - n or j == n:
+                print("first",j)
+            elif i -n == j -n and j > i:
+                print("sec",j)
+            else:
+                print(i)
+        elif n <= 0:
+            
+            
+           
+            x = abs(n-i)
+            y = abs(n-j)
+            
+            if x > y or y == n:
+                print("first",j)
+
+            elif x == y  and  abs(i) > abs(j):
+                print("second",i)
+            else:
+                print("last",i)
+    
     else:
         return n
 
