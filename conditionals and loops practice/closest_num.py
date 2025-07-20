b1d7c97db62816e33b386119c91af7d54317193e
umairmalik7 <m.umair4983@gmail.com>
2025-07-20 12:46:59 +0500

    if&loops: closest number

--- conditionals and loops practice/closest_num.py
+++ conditionals and loops practice/closest_num.py
@@ -1,45 +1,17 @@
 import math
-
-num = int(input("Enter int: "))
-mod = int(input("Enter mod: "))
-def closestNumber( n, m):
+class Solution:
+    def closestNumber(self, n, m):
         # code here 
-    if m != 0:
-        for i in range(n,n-100000,-1):
-            if i % m == 0:
-                print(f"the value of i = {i}")
-                y  = math.isclose(i,n,rel_tol=0.5)
-                if y == True: break
-        for j in range(n,n+100000):
-            if j % m == 0:
-                print(f"the value of j = {j}")
-                x = math.isclose(j,n,rel_tol=1)
-                if x == True: break
+        q = n // m
         
-        if n > 0:
-            if n - i > j - n or j == n:
-                print("first",j)
-            elif i -n == j -n and j > i:
-                print("sec",j)
-            else:
-                print(i)
-        elif n <= 0:
-            
-            
-           
-            x = abs(n-i)
-            y = abs(n-j)
-            
-            if x > y or y == n:
-                print("first",j)
-
-            elif x == y  and  abs(i) > abs(j):
-                print("second",i)
-            else:
-                print("last",i)
-    
-    else:
-        return n
-
-closestNumber(num, mod)
-
+        x = q * m
+        
+        a = m * (q + 1)
+       
+        
+        if abs(n-x) < abs(n - a):
+            return x
+        elif abs(n-x) >  abs(n-a) :
+            return a
+        else:
+            return x if abs(x) > abs(a) else a
