9d7f9bfb28f8693cdfef084eec7a716697619732
umairmalik7 <m.umair4983@gmail.com>
2025-07-21 12:37:59 +0500

    Easy: Dice Problem

---
+++ conditionals and loops practice/19_dice_problem.py
@@ -0,0 +1,17 @@
+#User function Template for python3
+
+class Solution:
+    def oppositeFaceOfDice(self, n):
+    	#code here 
+    	if n == 6:
+    	    return 1
+    	elif n == 1:
+    	    return 6
+    	elif n == 2:
+    	    return 5
+    	elif n == 5:
+    	    return 2
+    	elif n == 3:
+    	    return 4
+    	elif n == 4:
+    	    return 3
