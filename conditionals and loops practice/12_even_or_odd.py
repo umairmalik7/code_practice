e7ed449f82414e2a8c2c22f76835e4596c076759
umairmalik7 <m.umair4983@gmail.com>
2025-07-21 13:19:00 +0500

    Medium : for loop-1

--- conditionals and loops practice/12_even_or_odd.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def isEven (self, n):
-        # code here 
-        if n % 2 == 0:
-            return True
-        else:
-            return False
---
+++ conditionals and loops practice/20_for_loop-1.py
@@ -0,0 +1,7 @@
+#User function Template for python3
+n = int(input())
+
+# Your code here
+for i in range(1,11):
+    res = n * i
+    print(res , end=" ")
