2be0dd874ff644303fe96b9e65ddb1891d8472f2
Umair Naveed <m.umair4983@gmail.com>
2025-07-21 12:55:38 +0500

    Update Even_Odd.py
    Delete file content to remove

--- conditionals and loops practice/Even_Odd.py
+++ conditionals and loops practice/Even_Odd.py
@@ -1,11 +1,2 @@
-#User function Template for python3
-# Take n input and print who wins
-num = int(input())
 
-def even_odd(a):
-    if a % 2 == 0:
-        print("Friend")
-    else:
-        print("You")
 
-even_odd(num)
