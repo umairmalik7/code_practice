4ab753f9957f166e61eb0a7adc631542ce76c418
Umair Naveed <m.umair4983@gmail.com>
2025-07-21 13:15:00 +0500

    delete all files

--- conditionals and loops practice/for_loop.py
+++
@@ -1,6 +0,0 @@
-def multiplicationTable(N):
-    #code here 
-    for i in range(1,11):
-        table = N * i
-        print(table , end=" ")
-     
--- conditionals and loops practice/if_statement.py
+++
@@ -1,7 +0,0 @@
-def friends_in_trouble(j_angry, s_angry):
-    if j_angry is True and s_angry is True:
-        return True
-    elif j_angry is False and s_angry is False:
-        return True
-    else:
-        return False
