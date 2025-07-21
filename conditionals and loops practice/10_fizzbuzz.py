2496818f32553a339e4087d499bec2f986ea3bb8
umairmalik7 <m.umair4983@gmail.com>
2025-07-21 12:27:55 +0500

    Easy: FizzBuzz program

---
+++ conditionals and loops practice/10_fizzbuzz.py
@@ -0,0 +1,13 @@
+#User function Template for python3
+num = int(input())
+def fizzy(a):
+    if a % 3 == 0 and a % 5 == 0:
+        print("FizzBuzz")
+    elif a % 3 == 0:
+        print("Fizz")
+    elif a % 5 == 0:
+        print("Buzz")
+    else:
+        print(a)
+        
+fizzy(num)
