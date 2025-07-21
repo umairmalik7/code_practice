ed0f4703a294274a147e48b4a6375394a203f4d6
umairmalik7 <m.umair4983@gmail.com>
2025-07-21 12:26:48 +0500

    Easy: Else statement

---
+++ conditionals and loops practice/9_else_statement.py
@@ -0,0 +1,11 @@
+#User function Template for python3
+num = int(input())
+def greater(a):
+    b = "Big"
+    n = "Number"
+    if a > 100:
+        print(b)
+    else: 
+        print(n)
+        
+greater(num)
