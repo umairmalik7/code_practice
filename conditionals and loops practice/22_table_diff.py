c3813882506e28351a3cac4b021ff83095a80a00
umairmalik7 <m.umair4983@gmail.com>
2025-07-26 11:32:55 +0500

    Medium : table difference

---
+++ conditionals and loops practice/22_table_diff.py
@@ -0,0 +1,16 @@
+#User function Template for python3
+n1 = int(input())
+n2 = int(input())
+
+# Your code here 
+
+for i in range(1,11):
+    a = n1 * i
+    b = n2 * i
+       
+    print(a-b, end=" ") 
+               
+
+
+
+
