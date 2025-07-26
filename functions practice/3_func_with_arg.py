ef0238d20dd64cd8e5d5ee80947d283f62ed3875
umairmalik7 <m.umair4983@gmail.com>
2025-07-26 12:13:29 +0500

    Easy: function with arguments

---
+++ functions practice/3_func_with_arg.py
@@ -0,0 +1,10 @@
+#User function Template for python3
+
+#Write the complete argumentFunction below.
+#The function should take two arguments a and b
+#The function should return a+b
+#code here
+def argumentFunction(a,b):
+    return a+b
+
+#Write the argumentFunction above.
