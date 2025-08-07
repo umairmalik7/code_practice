08b372c702dc803b102b54c2bd5d678d8ccf81c0
umairmalik7 <m.umair4983@gmail.com>
2025-08-07 23:48:09 +0500

    its not easy: plus one

---
+++ lists practice/16_plus_one.py
@@ -0,0 +1,16 @@
+arr = [5,7,6]
+
+def addOne(arr):
+    if arr[-1] < 9:
+        arr[-1] += 1
+        print(arr)
+    else:
+        for i in range(-1,0,-1):
+            
+    
+
+    
+    
+    
+
+addOne(arr)
