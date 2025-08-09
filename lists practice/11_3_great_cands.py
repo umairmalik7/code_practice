8f23d53a623d99cda47aa0f464c28e8e498c5590
umairmalik7 <m.umair4983@gmail.com>
2025-08-09 23:11:26 +0500

    still solving

--- lists practice/11_3_great_cands.py
+++ lists practice/11_3_great_cands.py
@@ -1,22 +1,25 @@
 #User function Template for python3
 
-arr = [10,3,5,5,4,4,2,3]
+arr = [-439 ,-284, -104 ,323, 554 ,382 ,139]
 def maxProduct( arr):
     # code here
+    arr2 = []
+    for i in arr:
+        a = abs(i)
+        arr2.append(a)
+
+    for i in range(len(arr)):
+        arr[i] = arr2[i]
+            
     arr.sort()
     arr.reverse()
     print(arr)
-
-    a = max(arr)
+    x = max(arr)
     for i in range(len(arr)):
         b =  arr[i]
         c = arr[i+1]
-        if b < a:
+        if b < x:
             if c <= b:
-                print(a*b*c)
+                print(x*b*c)
                 break
-
-            
-        
-            
 maxProduct(arr)
