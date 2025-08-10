dbd761e2648e7f878018b7f118d44834f397738e
umairmalik7 <m.umair4983@gmail.com>
2025-08-10 22:17:26 +0500

    not done

--- lists practice/11_3_great_cands.py
+++ lists practice/11_3_great_cands.py
@@ -3,23 +3,33 @@
 arr = [-439 ,-284, -104 ,323, 554 ,382 ,139]
 def maxProduct( arr):
     # code here
-    arr2 = []
-    for i in arr:
-        a = abs(i)
-        arr2.append(a)
-
-    for i in range(len(arr)):
-        arr[i] = arr2[i]
-            
     arr.sort()
+    x = min(arr)
+    print("smallest value",x)
+    for i in range(1,len(arr)):
+        y  = arr[i]
+        print("2nd smallest value",y)
+        if y <= x:
+            pro2 = x*y
+            break
+    
+    
     arr.reverse()
-    print(arr)
-    x = max(arr)
+
+    a = max(arr)
+    print("maximum value", a)
+    pro3 = a*pro2
     for i in range(len(arr)):
         b =  arr[i]
         c = arr[i+1]
-        if b < x:
+        if b < a:
             if c <= b:
-                print(x*b*c)
+                pro1 = a*b*c
                 break
+                
+    if pro1 > pro3:
+        print("pro1", pro1)
+    else:
+        print("pro3",pro3)
+
 maxProduct(arr)
