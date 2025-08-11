38517140579e575b08ff85258e10f56937a9634a
umairmalik7 <m.umair4983@gmail.com>
2025-08-11 16:41:51 +0500

    Easy: Three great candidates

--- lists practice/11_3_great_cands.py
+++ lists practice/11_3_great_cands.py
@@ -1,35 +1,26 @@
 #User function Template for python3
 
-arr = [-439 ,-284, -104 ,323, 554 ,382 ,139]
-def maxProduct( arr):
-    # code here
-    arr.sort()
-    x = min(arr)
-    print("smallest value",x)
-    for i in range(1,len(arr)):
-        y  = arr[i]
-        print("2nd smallest value",y)
-        if y <= x:
-            pro2 = x*y
-            break
-    
-    
-    arr.reverse()
-
-    a = max(arr)
-    print("maximum value", a)
-    pro3 = a*pro2
-    for i in range(len(arr)):
-        b =  arr[i]
-        c = arr[i+1]
-        if b < a:
-            if c <= b:
-                pro1 = a*b*c
+class Solution:
+    def maxProduct(self, arr):
+        # code here
+        arr.sort()
+        x = min(arr)
+        for i in range(1,len(arr)):
+            y  = arr[i]
+            if y >= x:
+                pro2 = x*y
                 break
-                
-    if pro1 > pro3:
-        print("pro1", pro1)
-    else:
-        print("pro3",pro3)
-
-maxProduct(arr)
+        arr.reverse()
+        a = max(arr)
+        for i in range(1,len(arr)):
+            b =  arr[i]
+            c = arr[i+1]
+            if b <=  a:
+                if c <= b:
+                    pro1 = a*b*c
+                    break
+        pro3 = a*pro2
+        if pro1 > pro3:
+            return pro1
+        else:
+            return pro3
