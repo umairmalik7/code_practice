a0e3669659a21a46e34324dd8c1c2f4a9a7c4394
umairmalik7 <m.umair4983@gmail.com>
2025-08-14 11:56:01 +0500

    Easy : string functions 1

---
+++ strings practice/3_str_functions_l.py
@@ -0,0 +1,17 @@
+def trim(str):
+    a = str.strip()
+    return  a
+
+
+def exists(str, x):
+    b = str.find(x)
+    return b
+
+def titleIt(str):
+    c = str.title()
+    return c
+
+
+def casesSwap(str):
+    d = str.swapcase()
+    return d
